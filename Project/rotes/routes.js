c5d3cc7f1a1d171acabcb01ae20581980076be0a
Isaque <isaqueestolanossi04@gmail.com>
2025-06-24 15:08:19 -0300

    Implement item, client, and employee addition routes with error handling; update HTML forms for new input names and add client/employee login pages.

--- Project/rotes/routes.js
+++ Project/rotes/routes.js
@@ -1,17 +1,72 @@
 import express from "express";
-import { Item } from "../data/database.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import { Item, Cliente, Funcionario } from "../data/database.js";
+
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("../../Project")); // pasta com HTML/JS/CSS
+app.use(express.static(path.join(__dirname, "..", "..", "Project")));
 
+//Home
+app.get("/", async (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "..", "..", "Project", "src", "index.html")
+  );
+});
+
+/
+
+//Adicionando ITEM
 app.post("/item", async (req, res) => {
-  const novoitem = await Item.create({
-    nome: req.body.nome,
-    quantidade: 10,
-    preco: 99,
-  });
-  console.log(req.body);
+  try {
+    const novoItem = await Item.create({
+      nome: req.body.nomeitem,
+      quantidade: req.body.amountitem,
+      preco: req.body.priceitem,
+    });
+    console.log("Criou Item");
+    console.log(req.body);
+  } catch (error) {
+    console.log(error);
+  }
+  res.redirect("/");
+});
+
+//Adicionando CLIENTE
+app.post("/cliente", async (req, res) => {
+  try {
+    const novoCliente = await Cliente.create({
+      nome: req.body.nomecliente,
+      email: req.body.emailcliente,
+      senha: req.body.senhacliente,
+    });
+    console.log("Criou Cliente");
+    console.log(req.body);
+  } catch (error) {
+    console.log(error);
+  }
+  res.redirect("/");
+});
+
+//Adionando Funcionário
+app.post("/funcionario", async (req, res) => {
+  try {
+    const novoFuncionario = await Funcionario.create({
+      nome: req.body.nomefuncionario,
+      email: req.body.emailfuncionario,
+      senha: req.body.senhafuncionario,
+    });
+    console.log("Criou Funcionario");
+    console.log(req.body);
+  } catch (error) {
+    console.log(error);
+  }
+  res.redirect("/");
 });
 
 app.listen(3000, () =>
